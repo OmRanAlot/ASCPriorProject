0565d582da5da5cc0c5509514ca86fae8f7801f6
Om Rana <omranamile@gmail.com>
2023-08-06 20:24:26 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+let menu = document.getElementById("menu");
+let button = document.getElementById("playButton");
+
+let lives = 0;
+
+
+
+// button.onclick = function(event){
+//     event.preventDefault();
+//     let userChoice = menu.value;
+
+//     if(userChoice=="easy"){
+//         lives =5;
+
+//     } else if(userChoice=="med"){
+//         lives=3;
+
+//     }else if(userChoice=="hard"){
+//         lives=1;
+
+//     }
+// }
+
+
